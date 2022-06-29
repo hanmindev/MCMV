9fd78cd827a2a2a466dc129f510fefadecf55517
Hanmin Kim <hanminkim123@gmail.com>
2022-06-29 16:39:21 -0700

    remove main

--- main.py
+++
@@ -1,9 +0,0 @@
-# this is for testing
-
-from import_file import BvhFileLoader
-
-file_loader = BvhFileLoader('data/nikkori/nene.bvh', 2.0, 'xyz')
-
-model = file_loader.get_model()
-animation = file_loader.get_animation()
-
